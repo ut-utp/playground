2521cf3722971b757fc21d97b0d652e33f74ddef
Rahul Butani <rr.butani@gmail.com>
2019-09-19 18:43:03 -0500

    _a_ solution

--- src/wires/conversions.rs
+++ src/wires/conversions.rs
@@ -4,6 +4,7 @@
 
 use super::{Wire, BitCountType, num_bytes};
 use core::convert::TryInto;
+use repeat_macros::repeat_with_n;
 
 /// Wires with 128 bits or fewer.
 pub trait FitsInU128 {}
@@ -38,11 +39,38 @@ macro_rules! repeated_impl  {
     };
 }
 
-repeated_impl_inner!{128, FitsInU128, T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T}
-repeated_impl_inner!{64, FitsInU64, T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T}
-repeated_impl_inner!{32, FitsInU32, T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T}
-repeated_impl_inner!{16, FitsInU16, T T T T T T T T T T T T T T T T}
-repeated_impl_inner!{8, FitsInU8, T T T T T T T T}
+
+
+// repeated_impl_inner!{128, FitsInU128, T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T}
+// repeated_impl_inner!{64, FitsInU64, T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T}
+// repeated_impl_inner!{32, FitsInU32, T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T T}
+// repeated_impl_inner!{16, FitsInU16, T T T T T T T T T T T T T T T T}
+// repeated_impl_inner!{8, FitsInU8, T T T T T T T T}
+
+// repeat_with_n!(128, N, impl FitsInU128 for Wire<{N as BitCountType}, {num_bytes(N as BitCountType)}> { });
+
+macro_rules! single_impl {
+    ($num:expr, $impl_ty:ty) => {
+        impl $impl_ty for Wire<{$num as BitCountType}, {num_bytes($num as BitCountType)}> {}
+    };
+}
+
+// repeat_with_n!(128, N, single_impl!(N, FitsInU128));
+// This is dumb and requires nightly since it uses `#![feature(proc_macro_hygiene)]`.
+fn ____() {
+    repeat_with_n!(128, N, { single_impl!(N, FitsInU128); });
+    repeat_with_n!(64,  N, { single_impl!(N, FitsInU64); });
+    repeat_with_n!(32,  N, { single_impl!(N, FitsInU32); });
+    repeat_with_n!(16,  N, { single_impl!(N, FitsInU16); });
+    repeat_with_n!(8,   N, { single_impl!(N, FitsInU8); });
+}
+// fn foo() {
+//     repeat_with_n!(128, n, println!("{:?}", n));
+//     repeat_with_n!(10, Y, {
+//             println!("{}", format!("{}", format!("{}{}{}{}", Y, Y, Y, Y)));
+//         });
+// }
+
 
 impl<const B: BitCountType, const S: usize> From<Wire<{B}, {S}>> for u8
 where Wire<{B}, {S}>: FitsInU8
